2ac0212fe18ded4b7721602d64d497a3d194c62e
anjwoc <anjwoc@naver.com>
2023-01-08 02:12:35 +0900

    first settings

---
+++ morethan-log.config.js
@@ -0,0 +1,63 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: 'morethanmin',
+    image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: 'backend engineer',
+    bio: '...',
+    email: 'anjwoc@gmail.com',
+    linkedin: '',
+    github: 'anjwoc',
+    instagram: '',
+  },
+  projects: [
+    // {
+    //   name: 'Untilled',
+    //   href: 'https://untilled.web.app'
+    // }
+  ],
+  // blog setting (required)
+  blog: {
+    title: 'anjwoc',
+    description: 'welcome to anjwoc log',
+    theme: 'dark' // ['light', 'dark', 'auto']
+  },
+
+  // CONFIG configration (required)
+  link: '',
+  since: 2022, // If leave this empty, current year will be used.
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: '', // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ['Blog', 'Website', 'Notion', 'Backend', 'Spring', 'Node.js'],
+  },
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.GOOGLE_MEASUREMENT_ID || ''
+    }
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || ''
+    }
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: 'anjwoc/notion-blog',
+      'issue-term': 'og:title',
+      label: '💬 Utterances',
+    }
+  },
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+module.exports = CONFIG
