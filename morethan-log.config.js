558a8d760c7f26fcb055e643d66dcb48f189c3ae
anjwoc <anjwoc@naver.com>
2023-01-08 03:20:38 +0900

    change theme

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: 'anjwoc',
     description: 'welcome to anjwoc log',
-    theme: 'dark' // ['light', 'dark', 'auto']
+    theme: 'auto' // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
